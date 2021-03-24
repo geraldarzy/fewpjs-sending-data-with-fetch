3666a375001ba21b3048dd1228190cb496e241a1
geraldarzy <geraldarzola@gmail.com>
2021-03-24 12:34:45 -0400

    commit tree e65886fddff38b51469baee12bfb53624843fe48

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+configurationObject = {
+    method:"POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle"
+      })
+}; 
+  
+fetch("http://localhost:3000/dogs",configurationObject).then(function(resp){return resp.json()}).then(function(json){console.log(json)}) .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  }); 
+
+  function submitData(inp){
+    let configObj = {
+        method:"POST",
+        heaeders: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(inp)
+    };
+
+    fetch("http://localhost:3000/users",configObj)
+  }
